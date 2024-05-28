33ad4df5dbcf50a0357449f8dec2c937e4c6bd60
Your Name <deudajm@gmail.com>
2024-05-28 10:57:13 +0800

    new ups

--- src/views/home.vue
+++ src/views/home.vue
@@ -17,8 +17,9 @@
                 alt="Shoes"
               />
             </figure>
-            <div class="card-body">
+            <div class="card-body p-3">
               <h2 class="card-title">Adobo</h2>
+              <p class="text-xs">OCT 12,2024</p>
             </div>
           </div>
           <div class="card w-52 bg-base-100 shadow-xl">
@@ -28,8 +29,48 @@
                 alt="Shoes"
               />
             </figure>
-            <div class="card-body">
-              <h2 class="card-title">Shoes!</h2>
+            <div class="card-body p-3">
+              <h2 class="card-title">Ginisa</h2>
+              <p class="text-xs">OCT 12,2024</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="py-2">
+      <h1 class="font-semibold py-2">All Recipes</h1>
+      <div class="flex justify-center sm:justify-start items-center">
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2"
+        >
+          <div class="card w-52 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="px-3 py-4">
+              <h2 class="card-title">Adobo</h2>
+              <span
+                class="text-xs font-semibold px-4 py-1 rounded-full text-blue-500 bg-blue-500/20"
+                >ni Jeric Banalnal</span
+              >
+            </div>
+          </div>
+          <div class="card w-52 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="px-3 py-4">
+              <h2 class="card-title">Ginisa</h2>
+              <span
+                class="text-xs font-semibold px-4 py-1 rounded-full text-blue-500 bg-blue-500/20"
+                >ni Jeric Banalnal</span
+              >
             </div>
           </div>
         </div>
