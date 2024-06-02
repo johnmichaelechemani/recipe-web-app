da9253831b9d56eed72fbcc1e72ff65be6ad7496
Your Name <deudajm@gmail.com>
2024-06-02 18:57:18 +0800

    add search btn

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="p-2">
     <Header />
+    <div class="flex justify-end gap-2 items-center my-2">
+      <input
+        type="search"
+        v-model="search"
+        placeholder="Search..."
+        class="input input-bordered sm:w-52 md:w-96 w-full"
+      />
+      <button class="btn btn-primary" @click="filter">
+        <Icon icon="bi:search" class="text-xl" />
+      </button>
+    </div>
     <AddRecipe />
     <YourRecipe />
     <AllRecipe />
   </div>
 </template>
 <script>
+import { ref } from "vue";
 import { Icon } from "@iconify/vue";
 import Header from "../components/header.vue";
 import AddRecipe from "../components/addRecipe.vue";
@@ -19,6 +31,13 @@ export default {
     AddRecipe,
     AllRecipe,
     YourRecipe,
+  },
+  setup() {
+    const search = ref("");
+    const filter = () => {
+      console.log(search.value);
+    };
+    return { search, filter };
   },
 };
 </script>
