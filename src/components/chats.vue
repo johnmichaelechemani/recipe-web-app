50e2f6691a0e15cb50d7258362c149405e8b7a6e
Your Name <deudajm@gmail.com>
2024-06-16 22:24:48 +0800

    new ups

--- src/components/chats.vue
+++ src/components/chats.vue
@@ -1,24 +1,38 @@
 <template>
-  <button class="btn rounded-full shadow" onclick="chatModal.showModal()">
-    <Icon icon="ant-design:message-filled" class="text-xl" />
-  </button>
+  <router-link to="yourChats">
+    <button class="btn rounded-full shadow" onclick="chatModal.showModal()">
+      <Icon icon="ant-design:message-filled" class="text-xl" />
+    </button>
+  </router-link>
 
   <dialog id="chatModal" class="modal">
     <div class="modal-box">
       <h3 class="font-bold text-xl">Chats</h3>
-      <hr class="border border-gray-400/10" />
+      <hr class="border border-gray-400/10 my-1" />
       <div class="flex justify-start items-center gap-2 my-2">
-        <router-link
-          to="yourChats"
-          class="btn btn-sm border border-none shadow text-blue-500 rounded-full"
-        >
-          Inbox
+        <router-link to="yourChats">
+          <button
+            class="rounded-full"
+            :class="
+              $route.path === '/yourChats'
+                ? 'btn btn-sm text-blue-500 shadow'
+                : 'hover:bg-blue-500/10 hover:text-blue-500 transition px-4 py-1.5 font-semibold text-sm'
+            "
+          >
+            Inbox
+          </button>
         </router-link>
-        <router-link
-          to="chefs"
-          class="hover:bg-blue-500/10 hover:text-blue-500 transition px-4 py-1.5 font-semibold text-sm rounded-full"
-        >
-          Chefs
+        <router-link to="chefs">
+          <button
+            class="rounded-full"
+            :class="
+              $route.path === '/chefs'
+                ? 'btn btn-sm text-blue-500 shadow'
+                : 'hover:bg-blue-500/10 hover:text-blue-500 transition px-4 py-1.5 font-semibold text-sm'
+            "
+          >
+            Chefs
+          </button>
         </router-link>
       </div>
       <div>
--- src/views/allChefs.vue
+++ src/views/allChefs.vue
@@ -1,1 +1,30 @@
-<template>allChefs</template>
+<template>
+  <div>
+    <div v-for="user in storedUsers" :key="user.id">
+      <div
+        @click="chatChef(user.id)"
+        class="flex justify-start items-center gap-2 cursor-pointer hover:bg-gray-500/20 transition p-1 rounded-md"
+      >
+        <div
+          class="avatar"
+          :class="user.status === 'online' ? 'online' : 'offline'"
+        >
+          <div class="w-10 rounded-full">
+            <img :src="user.userPhotoURL" />
+          </div>
+        </div>
+        <div>
+          <h1 class="text-sm font-medium">{{ user.userName }}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const storedUsers = JSON.parse(localStorage.getItem("users"));
+console.log(storedUsers);
+const chatChef = (id) => {
+  console.log(id);
+};
+</script>
