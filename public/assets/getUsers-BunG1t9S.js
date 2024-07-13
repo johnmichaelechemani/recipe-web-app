b8ea0d22f24dac84ae3c4fcbd6ee82a989b3b359
Your Name <deudajm@gmail.com>
2024-07-13 19:39:48 +0800

    new deps

---
+++ public/assets/getUsers-BunG1t9S.js
@@ -0,0 +1,1 @@
+import{l as c,k as e}from"./index-B2BqLNcl.js";function g(){const t=c(),r=e(t.currentUser),{uid:o,photoURL:l,displayName:i}=r.value,u=o,s=e([]),a=JSON.parse(localStorage.getItem("users")).filter(n=>n.id!==u);return s.value=a,{storedUsers:s}}export{g};
