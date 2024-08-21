f73cf7c0011b444909dcb7771c158ec3e68e7cd0
Your Name <deudajm@gmail.com>
2024-08-21 18:15:18 +0800

    "Updated Search.vue component: changed input type to 'search', added filtering to user list, and integrated ChatModal component"

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -23,7 +23,7 @@
       </form>
       <div class="my-3 flex justify-start gap-2 text-sm items-center">
         <input
-          type="text"
+          type="search"
           v-model="search"
           placeholder="Search"
           class="input input-bordered input-md w-full rounded-full"
@@ -41,33 +41,65 @@
           <Icon icon="lets-icons:search-light" class="text-3xl" />
         </button>
       </div>
-      <div>
-        <div v-for="user in storedUsers" :key="user.id">
-          <UsersChatHeads
-            :user="user"
-            :yourChat="chefsChat"
-            :formatTime="Time"
-            :latestMessages="latestMessages"
-            :getChatId="getChatId"
-            :userId="userId"
-            :isSender="isSender"
-            :timestamp="timestamp"
-          />
+      <div class="min-h-96">
+        <div class="my-2">
+          <span
+            class="font-semibold text-xs text-gray-500 bg-gray-500/10 px-3 py-0.5 shadow-sm rounded-full"
+            >Users</span
+          >
         </div>
+        <div>
+          <div
+            v-for="user in filteredUsers"
+            v-if="filteredUsers.length !== 0"
+            :key="user.id"
+          >
+            <UsersChatHeads
+              :user="user"
+              :yourChat="chefsChat"
+              :formatTime="Time"
+              :latestMessages="latestMessages"
+              :getChatId="getChatId"
+              :userId="userId"
+              :isSender="isSender"
+              :timestamp="timestamp"
+            />
+          </div>
+          <div v-else class="text-sm font-medium text-red-500">
+            User not found
+          </div>
+        </div>
+        <dialog id="openChat" class="modal modal-bottom sm:modal-middle">
+          <ChatModal
+            :userId="userId"
+            :messages="messages"
+            :selectedUser="selectedUser"
+            :userPhoto="userPhoto"
+            :userName="userName"
+            :isSendMessageLoading="isSendMessageLoading"
+            :isLoading="isLoading"
+            :formatTime="Time"
+            :sendMessage="sendMessage"
+            :filteredMessages="filteredMessages"
+            v-model="newMessage"
+          />
+        </dialog>
       </div>
     </div>
   </dialog>
 </template>
 <script setup>
-import { ref, defineComponent } from "vue";
+import { ref, defineComponent, computed } from "vue";
 import { Icon } from "@iconify/vue";
 import { ChatFuntions } from "../scripts/ChatFunctions.js";
 import UsersChatHeads from "../components/usersChatHeads.vue";
+import ChatModal from "./ChatModal.vue";
 
 const search = ref("");
 
 const component = defineComponent({
   UsersChatHeads,
+  ChatModal,
 });
 
 const handleSearch = () => {
@@ -75,15 +107,31 @@ const handleSearch = () => {
   modal.showModal();
   console.log("hey");
 };
-const searchUsers = () => {
-  console.log(search.value);
-};
+const filteredUsers = computed(() => {
+  if (search.value === "") {
+    return storedUsers.value;
+  } else {
+    return storedUsers.value.filter((user) =>
+      user.userName.toLowerCase().includes(search.value.toLowerCase())
+    );
+  }
+});
+
 const {
   Time,
   getChatId,
   chefsChat,
+  sendMessage,
   userId,
+  newMessage,
+  messages,
+  isLoading,
+  isSendMessageLoading,
+  filteredMessages,
+  selectedUser,
   timestamp,
+  userPhoto,
+  userName,
   latestMessages,
   isSender,
   storedUsers,
