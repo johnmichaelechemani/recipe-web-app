ef66a0b045ca10de61c9b154bc25586b2f04b40e
Your Name <deudajm@gmail.com>
2024-08-23 17:49:16 +0800

    "Refactored Home.vue: added max-width container, rearranged layout, and added new elements"

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,28 +1,30 @@
 <template>
-  <div v-if="!LoadingWebsite" class="p-2" v-motion-fade>
-    <Header />
+  <div v-if="!LoadingWebsite" class="p-2 flex justify-center items-center" v-motion-fade>
+    <div class=" max-w-6xl">
+      <Header />
 
-    <div class="flex justify-end gap-2 items-center my-2">
-      <input
-        type="search"
-        v-model="search"
-        placeholder="Search..."
-        class="input input-bordered sm:w-52 md:w-96 w-full"
-      />
-      <button
-        :disabled="search.length === 0"
-        class="btn btn-primary"
-        @click="filter"
-      >
-        <Icon icon="bi:search" class="text-xl" />
-      </button>
+      <div class="flex justify-end gap-2 items-center my-2">
+        <input
+          type="search"
+          v-model="search"
+          placeholder="Search..."
+          class="input input-bordered sm:w-52 md:w-96 w-full"
+        />
+        <button
+          :disabled="search.length === 0"
+          class="btn btn-primary"
+          @click="filter"
+        >
+          <Icon icon="bi:search" class="text-xl" />
+        </button>
+      </div>
+      <div class="flex justify-start items-center gap-2 mt-5">
+        <AddRecipe />
+        <Chats />
+      </div>
+      <YourRecipe />
+      <AllRecipe :searchQuery="search" />
     </div>
-    <div class="flex justify-start items-center gap-2 mt-5">
-      <AddRecipe />
-      <Chats />
-    </div>
-    <YourRecipe />
-    <AllRecipe :searchQuery="search" />
   </div>
   <div v-else>
     <Loading />
