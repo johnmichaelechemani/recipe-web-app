8b8ab1f65c4583a2c37fb816487dcb26fa1fd091
Your Name <deudajm@gmail.com>
2024-10-26 08:45:30 +0800

    add message route

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,11 @@ const router = createRouter({
       component: () => import("../views/login.vue"),
     },
     {
+      path: "/messages",
+      name: "messages",
+      component: () => import("../views/message.vue"),
+    },
+    {
       path: "/crud",
       name: "crud",
       component: () => import("../views/crud.vue"),
---
+++ src/views/message.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-2 flex justify-start items-center" v-motion-fade>
+    <div class="max-w-6xl min-w-2xl"><h1 class="text-2xl font-semibold">Chats</h1></div>
+  </div>
+</template>
