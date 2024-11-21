a21befdcaf0563c042ac54dae900e2db43f0f38e
Your Name <deudajm@gmail.com>
2024-11-21 21:49:13 +0800

    "Refactor login.vue: updated layout, styling, and content"

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="fixed top-2 left-1/2 transform -translate-x-1/2 z-10">
-    <div>
-      <div class="px-5 py-2 rounded-full border border-gray-300/10 shadow">
-        <h1 class="text-base text-neutral font-semibold text-center">
-          Recipe App
+  <div class="flex justify-center items-center h-dvh w-full">
+    <div
+      class="grid grid-cols-1 mx-5 max-w-6xl place-items-center gap-8 sm:grid-cols-2 w-full text-neutral"
+    >
+      <div>
+        <h1 class="sm:text-7xl my-3 text-5xl font-extrabold">
+          Post recipe & message your <span class="text-primary">chefs</span>
         </h1>
+        <div>
+          <span class="text-sm"
+            >By: <span class="font-semibold">John Michael Echemani</span></span
+          >
+        </div>
       </div>
-      <span class="text-xs flex justify-center py-2 text-blue-500 italic"
-        >John Michael D. Echemani</span
-      >
-    </div>
-  </div>
-  <div class="flex items-center h-screen justify-center text-neutral">
-    <div class="px-5 py-5 border border-gray-500/50 rounded-xl shadow-lg">
-      <h1 class="text-3xl font-semibold py-2">Log in please... 🥺</h1>
-      <button
-        @click="signInWithGoogle()"
-        class="btn items-center justify-between w-full gap-2 my-5 rounded-full"
-      >
-        <Icon icon="devicon:google" class="text-xl" />
-        Google
-      </button>
-      <p class="text-xs py-2 font-medium">
-        You only need <span class="text-blue-500">GOOGLE</span> no need more than
-        that.
-      </p>
+      <div class="px-5 py-5 border border-gray-500/20 rounded-2xl shadow-lg">
+        <h1 class="text-3xl font-semibold py-2">Log in 🥺</h1>
+        <button
+          @click="signInWithGoogle()"
+          class="btn items-center justify-between w-full gap-2 my-5 rounded-full"
+        >
+          <Icon icon="devicon:google" class="text-xl" />
+          Google
+        </button>
+        <p class="text-sm py-2 font-medium">
+          You only need <span class="text-primary">GOOGLE</span> no need more
+          than that.
+        </p>
+      </div>
     </div>
   </div>
 </template>
