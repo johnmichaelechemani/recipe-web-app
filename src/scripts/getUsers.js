ccaf81afef16acf20a0f3040fa50c922046ace2d
Your Name <deudajm@gmail.com>
2025-01-14 18:42:16 +0800

    `feat: updated getUsers.js to use Firebase Firestore and refactored code`

--- src/scripts/getUsers.js
+++ src/scripts/getUsers.js
@@ -1,19 +1,48 @@
 import { getAuth } from "firebase/auth";
-import { ref } from "vue";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { ref, onMounted } from "vue";
+
 export function getUsers() {
   const auth = getAuth();
+  const firestore = getFirestore();
+
   const user = ref(auth.currentUser);
-  const { uid, photoURL, displayName } = user.value;
-  const userId = uid;
-
   const storedUsers = ref([]);
-  const users = JSON.parse(localStorage.getItem("users"));
 
-  const filteredUsers = users.filter((storedUser) => storedUser.id !== userId);
-  storedUsers.value = filteredUsers;
-  //console.log(storedUsers.value);
-  //console.log(userId);
+  const fetchAndStoreUsers = async () => {
+    if (!user.value) return;
+
+    const { uid: userId } = user.value;
+
+    try {
+      const usersCollection = collection(firestore, "users");
+      const querySnapshot = await getDocs(usersCollection);
+
+      const users = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      const filteredUsers = users.filter(
+        (storedUser) => storedUser.id !== userId
+      );
+
+      localStorage.setItem("users", JSON.stringify(users));
+
+      storedUsers.value = filteredUsers;
+
+      console.log("Stored Users:", storedUsers.value);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  onMounted(() => {
+    fetchAndStoreUsers();
+  });
+
   return {
     storedUsers,
+    fetchAndStoreUsers,
   };
 }
